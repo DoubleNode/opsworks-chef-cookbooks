6b4533b7972c5d59dde63289c197d0bc92b8e41e
Jerry C <jerry@mainstreetdata.net>
2016-06-22 23:14:26 -0500

    fix logic error

--- kohana/recipes/permissions.rb
+++ kohana/recipes/permissions.rb
@@ -1,11 +1,7 @@
-# Get the base directory that OpsWorks will deploy to
-deploy_to =node[:deploy][:deploy_to]
-
 # chown the latest release directory
 ruby_block "chown_release_directory" do
   block do
-    lastRelease = `ls -t #{deploy_to}/releases/ | head -n 1`
-    %x[chmod 775 #{deploy_to}/releases/#{lastRelease}/httpdocs/application/logs]
-    %x[chmod 775 #{deploy_to}/releases/#{lastRelease}/httpdocs/application/cache]
+    %x[chmod 775 #{deploy[:deploy_to]}/current/httpdocs/application/logs]
+    %x[chmod 775 #{deploy[:deploy_to]}/current/httpdocs/application/cache]
   end
 end
