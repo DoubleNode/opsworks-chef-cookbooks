f1db428e77d823e48502574fc652148da46d493e
Jerry C <jerry@mainstreetdata.net>
2016-06-22 23:22:48 -0500

    still not quite right

--- kohana/recipes/permissions.rb
+++ kohana/recipes/permissions.rb
@@ -1,7 +1,11 @@
 # chown the latest release directory
-ruby_block "chown_release_directory" do
-  block do
-    %x[chmod 775 #{deploy[:deploy_to]}/current/httpdocs/application/logs]
-    %x[chmod 775 #{deploy[:deploy_to]}/current/httpdocs/application/cache]
+node[:deploy].each do |application, deploy|
+
+  ruby_block "chown_release_directory" do
+    block do
+      %x[chmod 775 #{deploy[:deploy_to]}/current/httpdocs/application/logs]
+      %x[chmod 775 #{deploy[:deploy_to]}/current/httpdocs/application/cache]
+    end
   end
 end
+
