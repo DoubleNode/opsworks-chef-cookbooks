30796d7fd792f6537051914a8f9773eb86aec45d
Jerry C <jerry@mainstreetdata.net>
2016-11-11 00:10:31 -0600

    use setafcl to fix kohana permissions

--- kohana/recipes/permissions.rb
+++ kohana/recipes/permissions.rb
@@ -1,11 +1,15 @@
-# chown the latest release directory
+# Set ACL rules to give proper permission to cache and logs
+
 node[:deploy].each do |application, deploy|
 
-  ruby_block "chown_release_directory" do
-    block do
-      %x[chmod 775 #{deploy[:deploy_to]}/current/httpdocs/application/logs]
-      %x[chmod 775 #{deploy[:deploy_to]}/current/httpdocs/application/cache]
-    end
+  script "update_acl" do
+    interpreter "bash"
+    user "root"
+    cwd "#{deploy[:deploy_to]}/current"
+    code <<-EOH
+    mount -o remount,acl /srv/www 
+    setfacl -R -m u:www-data:rwX -m u:ubuntu:rwX *
+    setfacl -dR -m u:www-data:rwx -m u:ubuntu:rwx *
+    EOH
   end
-end
 
